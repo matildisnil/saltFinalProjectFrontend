ae5671a1b90432a9141800af4bf73535537f7470
Salt Developer <matilda.lindahl@gmail.com>
2022-07-24 11:18:28 +0200

    some minor changes with the node_env

--- client/src/environment.js
+++ client/src/environment.js
@@ -1,5 +1,5 @@
 const REACT_APP_PATH_TO_SERVER = process.env.NODE_ENV === 'development' ? 
 'http://localhost:3001' :
-'http://salt-finalproject-backend.herokuapp.com'
+'https://salt-finalproject-backend.herokuapp.com'
 
 export default REACT_APP_PATH_TO_SERVER;
