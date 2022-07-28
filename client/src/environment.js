bcaa0309ec0d11c5299334b9b4374249cb8335c1
Salt Developer <matilda.lindahl@gmail.com>
2022-07-28 14:25:16 +0200

    finetuning

--- client/src/environment.js
+++ client/src/environment.js
@@ -1,6 +1,6 @@
 const REACT_APP_PATH_TO_SERVER = process.env.NODE_ENV === 'development' ? 
 'http://localhost:3001' :
-'https://activity-knot.herokuapp.com/'
+'https://activity-knot.herokuapp.com'
 
 
 //'https://salt-finalproject-backend.herokuapp.com'
